5c486a101d8f214fba1d41e43a2651f88d7e5ce2
Axelzu <alexanderky08@gmail.com>
2023-06-25 16:49:51 -0500

    primer commit

--- Practica2/CaracteresEnlazados.c
+++ Practica2/CaracteresEnlazados.c
@@ -6,8 +6,7 @@ int main (int argc, char *argv[])
     char c1,c2,c3;
     printf("Ingrese 3 caracteres: \n");
     scanf("%c %c %c", &c1, &c2, &c3);
-    /*-IMPRESIÓN-------*/
-
+    
     printf("%c-%c-%c \n", c1,c2,c3);
     system("pause");
     return 0;
